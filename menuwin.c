e83ec943ffafdc1ecf4b13a63eb5ab3f8d6e00bb
Sergio Costas <raster@rastersoft.com>
2013-09-10 23:35:43 +0200

    Remade launcher button

--- menuwin.c
+++ menuwin.c
@@ -431,23 +431,12 @@ void menuwin_paint_launcher() {
 	cairo_stroke(key_win.cr);
 	
 	
-	cairo_rectangle(key_win.cr,-0.5,-0.5,0.25,0.25);
-	cairo_fill(key_win.cr);
-	cairo_rectangle(key_win.cr,-0.5,-0.15,0.25,0.25);
-	cairo_fill(key_win.cr);
-	cairo_rectangle(key_win.cr,-0.5,0.2,0.25,0.25);
-	cairo_fill(key_win.cr);
-	cairo_rectangle(key_win.cr,-0.15,-0.5,0.25,0.25);
-	cairo_fill(key_win.cr);
-	cairo_rectangle(key_win.cr,-0.15,-0.15,0.25,0.25);
-	cairo_fill(key_win.cr);
-	cairo_rectangle(key_win.cr,-0.15,0.2,0.25,0.25);
-	cairo_fill(key_win.cr);
-	cairo_rectangle(key_win.cr,0.2,-0.5,0.25,0.25);
-	cairo_fill(key_win.cr);
-	cairo_rectangle(key_win.cr,0.2,-0.15,0.25,0.25);
-	cairo_fill(key_win.cr);
-	cairo_rectangle(key_win.cr,0.2,0.2,0.25,0.25);
+	cairo_rectangle(key_win.cr,-0.5,-0.30,0.25,0.25);
+	cairo_rectangle(key_win.cr,-0.5,0.05,0.25,0.25);
+	cairo_rectangle(key_win.cr,-0.15,-0.30,0.25,0.25);
+	cairo_rectangle(key_win.cr,-0.15,0.05,0.25,0.25);
+	cairo_rectangle(key_win.cr,0.2,-0.30,0.25,0.25);
+	cairo_rectangle(key_win.cr,0.2,0.05,0.25,0.25);
 	cairo_fill(key_win.cr);
 	cairo_restore(key_win.cr);
 }

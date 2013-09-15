65cd361423bedde710e52c31752ced6b7150ea5a
Sergio Costas <raster@rastersoft.com>
2013-09-15 19:40:35 +0200

    test 2

--- actions.c
+++ actions.c
@@ -319,7 +319,7 @@ void action_key(xcb_generic_event_t *e) {
 	}
 
 	// SHUTDOWN
-	if (((ee->detail==135)&&(!(ee->state&XCB_MOD_MASK_CONTROL))&&((ee->state&XCB_MOD_MASK_1)))||(ee->detail==124)) {
+	if ((ee->detail==124)||((ee->detail==135)&&(!(ee->state&XCB_MOD_MASK_CONTROL))&&((ee->state&XCB_MOD_MASK_1)))) {
 		printf("Shutdown\n");
 		if(shutdown_win.cache->mapped==0) {
 			printf("Show shutdown\n");
